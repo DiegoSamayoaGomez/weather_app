97b6f63c37980d0409ceb577156856a68501adec
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2025-03-13 23:38:41 -0600

    Fetch the url and return the weather data based on the location

--- src/getData.js
+++ src/getData.js
@@ -1,2 +1,31 @@
-//Take the location and send id to the API
-import { apiKey } from "./apiKey.js";
+//Take the location from displayData and form an URL with the api KEY as well
+import { apiKey } from "./apiKey";
+
+export const getDataFunc = function getDataFunc() {
+  const getApiKey = () => {
+    return apiKey;
+  };
+
+  const getLocation = async (location) => {
+    // Form the URL with location and the key
+    let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=${getApiKey()}&contentType=json`;
+    try {
+      // Fetch the data and store it
+      const response = await fetch(url, { mode: "cors" });
+      console.log("Fetching finished");
+      // Check if there is an error (A message different from 20X)
+      if (!response.ok) {
+        throw new Error(`NOT OK ${response.status}`);
+      }
+      console.log("Not present errors");
+      // Otherwise return the response a JSON
+      const convertedResponse = await response.json();
+      return convertedResponse;
+    } catch (error) {
+      // If there is something wrong, send an error message as the return value
+      return `There is an error in GetData: ${error.message}`;
+      //console.error(`There is an error in GetData: ${error.message}`);
+    }
+  };
+  return { getLocation };
+};
