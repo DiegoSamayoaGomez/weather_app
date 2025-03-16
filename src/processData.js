fd7178d088cc3bd560b5194184eaab69d535f35b
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2025-03-15 22:35:10 -0600

    Send filtered data as an object

--- src/processData.js
+++ src/processData.js
@@ -1,16 +1,27 @@
 import { showDataFunc } from "./displayData";
 
-export async function getRawData(object) {
+export async function getRawData(rawData) {
   console.log("Getting RAW data");
-  return filterRawData(await object);
+  return filterRawData(await rawData);
 }
 
-export function filterRawData(object) {
+export function filterRawData(rawData) {
+  console.log("Complete data");
+  console.log(rawData);
   console.log("Filtering data");
-  //console.log(object);
-  const address = object.address;
-  const description = object.description;
-  showDataFunc(address, description);
+  const filteredData = {
+    completeAddres: rawData.resolvedAddress,
+    temperatureCelsius: rawData.currentConditions.temp,
+    description: rawData.description,
+    maxTemp: rawData.days[0].tempmax,
+    minTemp: rawData.days[0].tempmin,
+    todayDate: rawData.days[0].datetime,
+    address: rawData.address,
+    timeType: rawData.timezone,
+    sunrise: rawData.currentConditions.sunrise,
+    sunset: rawData.currentConditions.sunset
+  };
+  showDataFunc(filteredData);
 
   //return { address, description };
 }
