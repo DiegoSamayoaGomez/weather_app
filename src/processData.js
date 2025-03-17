53010ee1737594ea6d33e060c99c61da44debd15
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2025-03-16 21:15:08 -0600

    Add commments to the code to ensure readability

--- src/processData.js
+++ src/processData.js
@@ -1,14 +1,19 @@
 import { showDataFunc } from "./displayData";
 
+// Receive the return value of fetching API
 export async function getRawData(rawData) {
   console.log("Getting RAW data");
+  // Send the object of data when its avaible
   return filterRawData(await rawData);
 }
 
+// Receive the object of data
 export function filterRawData(rawData) {
   console.log("Complete data");
   console.log(rawData);
   console.log("Filtering data");
+  // Extract required data and ignore the rest
+  // Create and object with the filtered data
   const filteredData = {
     completeAddres: rawData.resolvedAddress,
     temperatureCelsius: rawData.currentConditions.temp,
@@ -21,7 +26,9 @@ export function filterRawData(rawData) {
     sunrise: rawData.currentConditions.sunrise,
     sunset: rawData.currentConditions.sunset
   };
+  // Send filtered data to the display to interpret it
   showDataFunc(filteredData);
 
+  // This function is no longer a factory function
   //return { address, description };
 }
