48dec265317c449a40f7fb38c749f25f04cb78a3
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2025-03-21 17:48:44 -0600

    Send information from the search bar to the api

--- src/displayData.js
+++ src/displayData.js
@@ -1,18 +1,5 @@
 import { getDataFunc } from "./getData.js";
 
-// This should be the form wich asks for a location, once triggered it will run the rest of the application
-export const askData = function () {
-  //const location = prompt("Gimme a location", "Guatemala");
-  const location = "Barillas";
-  getDataFunc().getLocation(location);
-};
-
-export const showError = function (messageError) {
-  const testDisplay = document.createElement("p");
-  testDisplay.textContent = messageError;
-  document.body.appendChild(testDisplay);
-};
-
 // Create a function which creates elements
 
 function elementGenerator(
@@ -29,39 +16,31 @@ function elementGenerator(
   return newElement;
 }
 
+// This should be the form wich asks for a location, once triggered it will run the rest of the application
+export const askData = function () {
+  //const location = prompt("Gimme a location", "Guatemala");
+  const location = "Barillas";
+  getDataFunc().getLocation(location);
+
+  // Select form
+  const searchBarForm = document.getElementById("searchBarForm");
+  console.log("Submitted");
+
+  searchBarForm.addEventListener("submit", (e) => {
+    // Stop form from sending data to a server
+    e.preventDefault();
+    const searchBar = document.getElementById("searchBar");
+    // Prevent from sending empty data to the api request
+    searchBar.value == ""
+      ? showError("Enter a valid location")
+      : console.log(searchBar.value);
+    // Clear input
+    searchBar.value = "";
+  });
+};
+
+// Once the function is called, show the current weather
 export const showDataFunc = function showDataFunc(filteredData) {
   console.log("Displaying filtrated data");
   console.log(filteredData);
-
-  const mainContainer = document.querySelector(".mainContainer");
-
-  const titleContainer = document.querySelector(".titleContainer");
-  mainContainer.appendChild(titleContainer);
-  const parTest = elementGenerator("p", "parTest", "TITLE", "");
-  titleContainer.appendChild(parTest);
-
-  /* 
-
-  // TITLE
-  const titleContainer = elementGenerator(
-    ".titleContainer",
-    "",
-    "CREATE P ELEMENT WICH WILL HAVE THE WEATHER APP TITLE",
-    ""
-  );
-  mainContainer.appendChild(titleContainer);
-
-  // CONTAINER
-  const container = elementGenerator(
-    ".container",
-    "",
-    "CREATE MAIN CONTENT",
-    ""
-  );
-  mainContainer.appendChild(container);
-
-  // search bar
-const searchBarGroup = elementGenerator("searchBarGroup", )
-  //
-  */
 };
